0a8ba166541f797ddc1883d5c9c033b0b5a7024b
Gianfranco Yosida Vilchez <gianfranco.yosida@gmail.com>
2017-05-21 15:38:31 -0300

    initial commit : maven project using spring boot

---
+++ src/main/java/com/melhoridade/MelhoridadeApplication.java
@@ -0,0 +1,14 @@
+package com.melhoridade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MelhoridadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelhoridadeApplication.class, args);
+	}
+
+}
